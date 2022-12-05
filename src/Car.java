c90dff4737d0b1b280fc4994b49be55a4deba31c
GYN1979 <galeeva.y@yandex.ru>
2022-12-05 23:35:20 +0400

    дз выполнено

---
+++ src/Car.java
@@ -0,0 +1,48 @@
+public class Car {
+    double engineVolume;
+    String brand;
+    String model;
+    String color;
+    String country;
+    int year;
+
+    Car(String brand, String model, int year, String country, String color, double engineVolume) {
+        if (brand == null){
+            this.brand = "default";
+    }else {
+            this.brand = brand;
+        }
+        if (model == null){
+            this.model = "default";
+        }else {
+            this.model = model;
+        }
+        if (country == null){
+            this.country= "default";
+        }else {
+            this.country = country;
+        }
+        if (year <= 0){
+            this.year= 2000;
+        }else {
+            this.year = year;
+        }
+        if (color == null){
+            this.color= "white";
+        }else {
+            this.color = color;
+        }
+        if (engineVolume <= 0){
+            this.engineVolume= 1.5;
+        }else {
+            this.engineVolume = engineVolume;
+        }
+
+    }
+    void Car () {
+        System.out.println(brand + model + ", " + year + " год выпуска, сборка в " + country + ", " + color + " цвета, объем двигателя - "
+               + engineVolume + " л.");
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.time.Period;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     //   задание
+        System.out.println("Задание");
+        person Maks = new person("Максим", -1988, "Минск", "бренд-менеджер");
+        Maks.person();
+        person Ann = new person("Анна", 1993, null, "методист образовательных программ");
+        Ann.person();
+        person Kate = new person("Катя", 1992, "Калининград", "продакт - менеджер");
+        Kate.person();
+        person Artem = new person("Артем", 1995, "Москва", "директор по развитию бизнеса");
+        Artem.person();
+// задание
+        System.out.println("Задание");
+        Car Lada = new Car (null,"Granta",2015, "России", "желтого", 1.7);
+        Lada.Car();
+        Car Audi = new Car ("Audi",null,2020, "Германии", "черный", 3.0);
+        Audi.Car();
+        Car BMW = new Car ("BMW","Z8",-2021, "Германии", "черный", 3.0);
+        BMW.Car();
+        Car Kia = new Car ("Kia","Sportage 4-го поколения",2018, null, "красный", 2.4);
+        Kia.Car();
+        Car Hyundai = new Car ("Hyundai","Avante",2016, "Южной Кореи", null, -1.6);
+        Hyundai.Car();
+    // задание
+        System.out.println( "Задание");
+
     }
 }
